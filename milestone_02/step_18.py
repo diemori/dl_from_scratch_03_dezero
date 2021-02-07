aece825c8bf488559db3c09eb42f2417865cd9e7
inwoo.ro <inwoo.ro@webtoonscorp.com>
2021-02-07 22:44:42 +0900

    step 18.1 - retain_grad

---
+++ milestone_02/step_18.py
@@ -0,0 +1,129 @@
+import numpy as np
+import weakref
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)}은(는) 지원하지 않습니다.")
+
+        self.data = data 
+        self.grad = None
+        self.creator = None
+        self.generation = 0
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1
+
+    def cleargrad(self):
+        self.grad = None
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = list()
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+
+            print(f"gys: {gys}, gxs: {gxs}")
+
+            if not isinstance(gxs, tuple):
+                gxs = (gxs, )
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    # funcs.append(x.creator)
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
+            
+        return True
+
+
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+
+        if not isinstance(ys, tuple):
+            ys = (ys, )
+
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+
+        for output in outputs:
+            output.set_creator(self)
+
+        self.inputs = inputs
+        self.outputs = [weakref.ref(output) for output in outputs]
+
+        return outputs if len(outputs) > 2 else outputs[0]
+
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y 
+
+    def backward(self, gy):
+        return gy, gy
+
+
+class Square(Function):
+    def forward(self, x):
+        y = x**2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data 
+        gx = 2*x * gy 
+        return gx 
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+def square(x0):
+    return Square()(x0)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+if __name__ == "__main__":
+    x0 = Variable(np.array(1.0))
+    x1 = Variable(np.array(1.0))
+    t = add(x0, x1)
+    y = add(x0, t)
+    y.backward()
+
+    print(y.grad, t.grad)
+    print(x0.grad, x1.grad)
