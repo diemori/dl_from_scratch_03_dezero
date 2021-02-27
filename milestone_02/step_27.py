03e8909db597c4556dd0767b6a5118247e6d8e8f
inwoo.ro <inwoo.ro@webtoonscorp.com>
2021-02-27 23:29:38 +0900

    step 27 - plot

--- milestone_02/step_27.py
+++ milestone_02/step_27.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 if '__file__' in globals():
@@ -8,11 +9,21 @@ from dezero import Variable
 from dezero import Sin
 from dezero.utils import get_dot_graph, plot_dot_graph
 
+def my_sin(x, threshold=0.0001):
+    y = 0
+    for i in range(10000):
+        c = (-1) ** i / math.factorial(2*i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+
+        if abs(t.data) < threshold:
+            break
+
+    return y
 
 x = Variable(np.array(np.pi/4))
-y = Sin.sin(x)
+y = my_sin(x, threshold=1e-20)
 y.backward()
 
-print(y.data)
-print(x.grad)
 
+plot_dot_graph(y, to_file="step27.png")
